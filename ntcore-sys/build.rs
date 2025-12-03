5a58f6d49ed97574d8addf95699725ff09f72e5e
DrewCodesBadly <dhruvak@outlook.com>
2025-12-02 22:33:26 -0500

    yet another genuinely mind-boggling windows issue
    literally HOW does this even happen.
    anyways it compiles on windows correctly now.

--- ntcore-sys/build.rs
+++ ntcore-sys/build.rs
@@ -16,10 +16,17 @@ fn main() {
     // println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
 
     let ntcoreffi_dir = PathBuf::from("ntcoreffi").canonicalize().expect("bad path");
-    println!(
-        "cargo:rustc-link-search={}",
-        ntcoreffi_dir.to_str().unwrap()
-    );
+    let ntcoreffi_dir_str = ntcoreffi_dir.to_str().unwrap();
+    // Windows inserts a mysterious "\\?\" before the paths, which causes
+    // clang to completely break. No idea why.
+    #[cfg(target_os = "windows")]
+    let ntcoreffi_dir_str = &ntcoreffi_dir_str[4..];
+    #[cfg(target_os = "windows")]
+    let dir = PathBuf::from(".").canonicalize().expect("bad path");
+    #[cfg(target_os = "windows")]
+    let dir_str = &dir.to_str().unwrap()[4..];
+
+    println!("cargo:rustc-link-search={}", ntcoreffi_dir_str);
 
     println!("cargo:rustc-link-lib={}", "ntcoreffi");
 
@@ -38,7 +45,14 @@ fn main() {
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         // makes the <> includes work properly. (why would they do this?)
-        .clang_args(["-I", ntcoreffi_dir.to_str().unwrap()])
+        .clang_args([
+            "-I",
+            ntcoreffi_dir_str,
+            #[cfg(target_os = "windows")]
+            "-I",
+            #[cfg(target_os = "windows")]
+            dir_str,
+        ])
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
